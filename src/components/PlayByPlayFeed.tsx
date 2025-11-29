301010c659e1c927baf0b36e3e419d4b4d2bbca3
Tyler Guo <tyleguo2004@gmail.com>
2025-11-28 19:16:35 -0800

    change period to quarter

--- src/components/PlayByPlayFeed.tsx
+++ src/components/PlayByPlayFeed.tsx
@@ -45,7 +45,7 @@ export default function PlayByPlayFeed({ actions }: Props) {
         <thead className="bg-gray-200 sticky top-0">
           <tr>
             <th className="p-2 text-center">Clock</th>
-            <th className="p-2 text-center">Period</th>
+            <th className="p-2 text-center">Quarter</th>
             <th className="p-2 text-center">Player</th>
             <th className="p-2 text-center">Description</th>
             <th className="p-2 text-center">Score</th>
